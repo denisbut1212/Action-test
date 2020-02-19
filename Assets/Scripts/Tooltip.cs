78014f0113d84899869af28e0197b54ec0a39a82
denisbut1212 <denisbut1212@gmail.com>
2020-02-19 07:46:56 +0300

    Tooltips

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+
+public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [TextArea] public string tooltipText;
+    public Color backgroundColor;
+    private GameObject _tooltip;
+    private TooltipManager _manager;
+    private void Start()
+    {
+        _tooltip = Instantiate(Resources.Load<GameObject>("Tooltip"), transform);
+        _manager = _tooltip.GetComponent<TooltipManager>();
+        _manager.SetText(tooltipText);
+        _manager.SetBackgroundColor(backgroundColor);
+        _tooltip.SetActive(false);
+    }
+
+    private void ShowTooltip()
+    {
+        var backgroundSize = new Vector2(_manager.GetText.preferredWidth, _manager.GetText.preferredHeight);
+        _manager.GetComponent<RectTransform>().sizeDelta = backgroundSize;
+        _tooltip.SetActive(true);
+    }
+
+    private void HideTooltip()
+    {
+        _tooltip.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ShowTooltip();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HideTooltip();
+    }
+}
---
+++ Assets/Scripts/TooltipManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipManager : MonoBehaviour
+{
+    [SerializeField] private Image background;
+    [SerializeField] private TMP_Text Text;
+    public TMP_Text GetText => Text;
+    public void SetText(string @string) => Text.text = @string;
+    public void SetBackgroundColor(Color color) => background.color = color;
+}
