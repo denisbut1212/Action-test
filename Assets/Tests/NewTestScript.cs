47ea5a2dcd6b6b456daaf229a1f08ae28982da8f
denisbut1212 <denisbut1212@gmail.com>
2020-04-09 17:21:21 +0300

    try unit test github action

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+        }
+    }
+}
