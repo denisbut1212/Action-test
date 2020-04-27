973fc712c7276466daa17dc7afb8566a3804e8fc
denisbut1212 <denisbut1212@gmail.com>
2020-04-27 13:55:11 +0300

    installer.iss

--- InnoSetup/getVersion.sh
+++ InnoSetup/getVersion.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 lastVersion=$(git tag | tail -1)
+cd InnoSetup
 { 
-	echo \#define MyAppVersion \"$lastVersion\" 
+	echo \#define MyAppVersion \"$lastVersion\"
 	cat installer.iss; 
 } > installer.iss.new
 mv installer.iss.new installer.iss
