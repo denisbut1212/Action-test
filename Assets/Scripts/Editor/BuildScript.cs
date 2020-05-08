2c15ca7d497c771198e6c2d7870d9301003e4c4c
denisbut1212 <denisbut1212@gmail.com>
2020-05-08 13:35:39 +0300

    build script

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class BuildScript
+{
+    public static void PerformBuild()
+    {
+        string[] scenes = {"Assets/Scenes/SampleScene.unity"};
+        BuildPipeline.BuildPlayer(scenes, "build/build.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+}
