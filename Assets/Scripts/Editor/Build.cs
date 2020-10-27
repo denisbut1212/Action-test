a274f41028c990e0e9b449ab00be88a9e94dfc97
denisbut1212 <denisbut1212@gmail.com>
2020-10-27 14:20:47 +0300

    fix

---
+++ Assets/Scripts/Editor/Build.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Editor
+{
+    public class Build
+    {
+        public static void Perform()
+        {
+            var scenes = EditorBuildSettings.scenes.Select(e => e.path).ToArray();
+            var targetGroup = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+            var initial = PlayerSettings.GetScriptingBackend(targetGroup);
+            PlayerSettings.SetScriptingBackend(targetGroup, ScriptingImplementation.IL2CPP);
+            PlayerSettings.SetIl2CppCompilerConfiguration(targetGroup, Il2CppCompilerConfiguration.Master);
+            BuildPipeline.BuildPlayer(scenes, "build/build.exe",
+                EditorUserBuildSettings.activeBuildTarget,
+                BuildOptions.None);
+            PlayerSettings.SetScriptingBackend(targetGroup, initial);
+        }
+    }
+}
--- InnoSetup/build.sh
+++ InnoSetup/build.sh
@@ -3,7 +3,7 @@
 unityPath="D:/Soft/Unity/2019.4.11f1/Editor/Unity.exe"
 lastVersion=$(git tag | tail -1)
 
-$unityPath -batchmode -quit -projectPath "../" -executeMethod Build.Perform
+$unityPath -batchmode -quit -executeMethod Build.Perform
 
 { 
 	echo \#define MyAppVersion \"$lastVersion\"
