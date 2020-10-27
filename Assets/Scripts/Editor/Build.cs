03099913d3118647d10d5c3951344d608ebcfa52
denisbut1212 <denisbut1212@gmail.com>
2020-10-27 17:35:00 +0300

    refactoring

--- Assets/Scripts/Editor/Build.cs
+++ Assets/Scripts/Editor/Build.cs
@@ -8,11 +8,12 @@ public class Build
         var scenes = EditorBuildSettings.scenes.Select(e => e.path).ToArray();
         var targetGroup = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
         var initial = PlayerSettings.GetScriptingBackend(targetGroup);
+        
         PlayerSettings.SetScriptingBackend(targetGroup, ScriptingImplementation.IL2CPP);
         PlayerSettings.SetIl2CppCompilerConfiguration(targetGroup, Il2CppCompilerConfiguration.Master);
         BuildPipeline.BuildPlayer(scenes, "build/build.exe",
             EditorUserBuildSettings.activeBuildTarget,
-            BuildOptions.AutoRunPlayer);
+            BuildOptions.None);
         PlayerSettings.SetScriptingBackend(targetGroup, initial);
     }
 }
