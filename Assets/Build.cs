574cf52521a52d663da3c36f29e799d1760b7c7f
denisbut1212 <denisbut1212@gmail.com>
2020-10-27 14:57:44 +0300

    changes in sh sctipt

---
+++ Assets/Build.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+
+using System.Linq;
+using UnityEditor;
+
+public class Build
+{
+    public static void Perform()
+    {
+        var scenes = EditorBuildSettings.scenes.Select(e => e.path).ToArray();
+        var targetGroup = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+        var initial = PlayerSettings.GetScriptingBackend(targetGroup);
+        PlayerSettings.SetScriptingBackend(targetGroup, ScriptingImplementation.IL2CPP);
+        PlayerSettings.SetIl2CppCompilerConfiguration(targetGroup, Il2CppCompilerConfiguration.Master);
+        BuildPipeline.BuildPlayer(scenes, "build/build.exe",
+            EditorUserBuildSettings.activeBuildTarget,
+            BuildOptions.AutoRunPlayer);
+        PlayerSettings.SetScriptingBackend(targetGroup, initial);
+    }
+}
+
+#endif
--- InnoSetup/build.sh
+++ InnoSetup/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 unityPath="D:/Soft/Unity/2019.4.11f1/Editor/Unity.exe"
 lastVersion=$(git tag | tail -1)
 
-$unityPath -batchmode -quit -executeMethod Build.Perform
+$unityPath -batchmode -projectPath "D:/Documents/GitHub/Action-test" -quit -executeMethod Build.Perform
 
 { 
 	echo \#define MyAppVersion \"$lastVersion\"
 	cat InnoSetup/installer.iss;
 } > installer.iss.new
 
-mv installer.iss.new installer.iss
+mv installer.iss.new InnoSetup/installer.iss
